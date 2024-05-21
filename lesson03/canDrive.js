a4a054c96c555c99938930e0cbbff9d38b11ef02
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-05-21 15:55:04 -0400

    added bonus assignment canDrive

---
+++ lesson03/canDrive.js
@@ -0,0 +1,12 @@
+const firstName = 'Brittni';
+const lastName = 'Rohrer';
+const age = 32;
+const drivingAge = 16;
+
+if(age >= drivingAge) {
+  console.log(firstName + ' ' + lastName);
+  console.log('Can Drive')
+  console.log((age - drivingAge)+ ' years of driving');
+} else for( i = 5; i > 0; i--) {
+  console.log(i);
+}
