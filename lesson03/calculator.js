625037103e885cbf88ac96f93311d597ec74580d
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-05-21 10:48:05 -0400

    added calculator

---
+++ lesson03/calculator.js
@@ -0,0 +1,12 @@
+const x = 10;
+const y = 21;
+const firstPhrase = 'Michigan';
+const lastPhrase = 'Life';
+
+console.log(x+y);
+console.log(firstPhrase+' '+lastPhrase);
+console.log(y/3);
+console.log(x*5.3);
+console.log(x**2);
+console.log(x+firstPhrase);
+console.log(x%4);
