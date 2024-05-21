d93a308354cd3b2469eb6bb5d0965e5c7d20e6c6
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-05-21 11:14:19 -0400

    added more math equations

--- lesson03/calculator.js
+++ lesson03/calculator.js
@@ -2,6 +2,7 @@ const x = 10;
 const y = 21;
 const firstPhrase = 'Michigan';
 const lastPhrase = 'Life';
+const z = '22';
 
 console.log(x+y);
 console.log(firstPhrase+' '+lastPhrase);
@@ -10,3 +11,6 @@ console.log(x*5.3);
 console.log(x**2);
 console.log(x+firstPhrase);
 console.log(x%4);
+console.log(x+z);
+console.log((2*x)-(y/7));
+console.log(((-x)*3)+(x**2)-(x%4));
