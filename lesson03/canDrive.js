20cfcde8774c90ef350888c277723412a7476ee1
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-05-22 13:44:11 -0400

    fixed syntax error

--- lesson03/canDrive.js
+++ lesson03/canDrive.js
@@ -5,7 +5,7 @@ const drivingAge = 16;
 
 if(age >= drivingAge) {
   console.log(firstName + ' ' + lastName);
-  console.log('Can Drive')
+  console.log('Can Drive');
   console.log((age - drivingAge)+ ' years of driving');
 } else for( i = 5; i > 0; i--) {
   console.log(i);
