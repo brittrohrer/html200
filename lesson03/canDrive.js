33af087363625e2258bb9539ef74ac17e3798dbb
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-05-22 23:23:17 -0400

    added string interpolation

--- lesson03/canDrive.js
+++ lesson03/canDrive.js
@@ -1,12 +1,10 @@
-const firstName = 'Brittni';
-const lastName = 'Rohrer';
-const age = 32;
+let firstName = 'Brittni';
+let lastName = 'Rohrer';
+let age = 32;
 const drivingAge = 16;
 
 if(age >= drivingAge) {
-  console.log(firstName + ' ' + lastName);
-  console.log('Can Drive');
-  console.log((age - drivingAge)+ ' years of driving');
+  console.log(`${firstName} ${lastName} : Can Drive : ${age-drivingAge} years of driving`);
 } else for( i = 5; i > 0; i--) {
   console.log(i);
 }
