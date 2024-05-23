8850c61bcc70fc7c0c20378b67c6d840f2cc6e6d
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-05-22 23:38:07 -0400

    added string interpolation

--- lesson03/cardDeck.js
+++ lesson03/cardDeck.js
@@ -3,6 +3,6 @@ let value = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
 
 for (let s = 0; s < 4; s++) {
   for (let v = 0; v < 13; v++) {
-    console.log(value[v] + ' of ' + suits[s]);
+    console.log(`${value[v]} of ${suits[s]}`);
   }
 }
