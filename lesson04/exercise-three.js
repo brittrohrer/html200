b3dc7f5d89233d83155a534751ea5011b5fd16c1
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-06-01 14:11:58 -0400

    added no jokers

--- lesson04/exercise-three.js
+++
@@ -1,6 +0,0 @@
-let potterJargonArray = ['patronous', 'horocrux', 'muggle', 'lumos', 'wand'];
-console.log(potterJargonArray);
-
-for(let i=0; i < potterJargonArray.length; i++) {
-  console.log(`${[i]} , ${potterJargonArray[i]}`);
-}
