26891bcdaff1afeda7e8e594d557dd8f6a224779
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-06-01 14:06:08 -0400

    added exercise 3

---
+++ lesson04/exercise-three.js
@@ -0,0 +1,6 @@
+let potterJargonArray = ['patronous', 'horocrux', 'muggle', 'lumos', 'wand'];
+console.log(potterJargonArray);
+
+for(let i=0; i < potterJargonArray.length; i++) {
+  console.log(`${[i]} , ${potterJargonArray[i]}`);
+}
