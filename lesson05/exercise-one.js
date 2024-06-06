b6a80b3aa74724029aac9b0b683bb6512546aed3
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-06-05 22:07:12 -0400

    added exercise 5.1

---
+++ lesson05/exercise-one.js
@@ -0,0 +1,18 @@
+function square() {
+    let input = prompt('give me a number');
+    let result = 0;
+
+    result = Number(input) * Number(input);
+
+    alert(result);
+}
+
+function multiplier() {
+    let number1 = prompt('Give me the first number');
+    let number2 = prompt('Give me a second number');
+    let result = 0;
+
+    result = Number(number1) * Number(number2);
+
+    alert(result);
+}
